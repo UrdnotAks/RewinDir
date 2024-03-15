e62d0c6a956205a56f660ffa0b7c8fd15372f39a
UrdnotAks <anishkumar77@proton.me>
2024-03-15 18:13:40 +0530

    feat: combined organize and restore operations into single script
    The organize_files and restore_files scripts are combined into a single script.
    This was achieved by adding a cmdline optional flag to restore files.
    
    This commit also names the project as 'rewindir'.

--- restore_files.py
+++ restore_files.py
@@ -1,25 +1,10 @@
-from shutil import move
 import copy_files as cp
 import pandas as pd
 import os
 
-import argparse
+import config as cf
 
-def parse_cmd_line_arguments():
-    '''
-    parses commandline arguments
-    '''
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('-s', '--src', 
-                        action='store', 
-                        required=True, 
-                        help='Source csv file that has the file path of images.')
-
-    args = parser.parse_args()
-    return args
-
-def restore_files(df):
+def _restore_files(df):
     '''
     restore files to its previous location if files paths were saved while
     transfering files
@@ -45,11 +30,13 @@ def read_df_from_file(src):
         return None
 
 
-if __name__ == '__main__':
-    args = parse_cmd_line_arguments()
-    df = read_df_from_file(args.src)
+def restore():
+    '''
+    Point of entry for restoring files operation
+    '''
+    df = read_df_from_file(cf.SRC_DIR)
     if df is not None:
-        restore_files(df)
-        cp.remove_empty_folders('/'.join(args.src.split('/')[:-1]))
+        _restore_files(df)
+        cp.remove_empty_folders('/'.join(cf.SRC_DIR.split('/')[:-1]))
     else:
         print('Restoration Failed. File specified is not valid.')
---
+++ rewindir.py
@@ -0,0 +1,35 @@
+from organize_files import organize
+from restore_files import restore
+from argument_parser import parse_cmd_line_arguments
+import config as cf
+
+
+def set_config(args):
+    '''
+    sets the config variables
+    '''
+    if args.restore:
+        cf.SRC_DIR = args.src
+    else:
+        cf.SRC_DIR = args.src if args.src.endswith('/') else args.src + '/'
+
+    cf.DST_DIR = args.dst if args.dst is not None else cf.SRC_DIR
+    cf.DST_DIR = cf.DST_DIR if cf.DST_DIR.endswith('/') else cf.DST_DIR + '/'
+    
+    cf.INTERACTIVE = args.interactive
+    cf.MOVE_FILES = args.move_files
+    cf.HANDLE_UNKNOWN_FILES = args.handle_unknown
+    if args.extra_file_types:
+        cf.ADDITIONAL_FILE_TYPES_TO_PROCESS += tuple([i for i in args.extra_file_types])
+    cf.TREAT_DECOMPRESSION_BOMB_WARNING_AS_ERROR = args.treat_pil_warnings_as_error
+    cf.NUM_FILES = args.num_files
+    cf.RECURSIVE = not args.no_recursive
+
+
+if __name__ == '__main__':
+    args = parse_cmd_line_arguments()
+    set_config(args)
+    if args.restore:
+        restore()
+    else:
+        organize()
